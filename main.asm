cb6e1fb83c1aa685f9e6272954fb08cd09a99416
romain.le-miere <romain.le-miere@epita.fr>
2022-04-30 01:43:08 +0200

    implements functionning gameloop

--- main.asm
+++ main.asm
@@ -1,19 +1,23 @@
 extern printf
 
 extern len, blind_copy, mutate_str
-extern str_fmt, int_fmt
+extern str_fmt, int_fmt, chr_fmt
 extern randint
 
 section .data
 	msg db "Hello, World",0
+	temp db "hangman", 0
 	buffsize dq 1024
 
 section .bss
-	copy resb 8 	; the pointer to the blind copy
-	tries resb 4	; the number of tries
-	wlen resb 4		; the length of the string
+	secret resq 1	; the pointer to the word 
+	blind resq 1 	; the pointer to the blind copy
+	tries resd 1	; the number of tries
+	wlen resd 1		; the length of the string
+	found resd 1	; the number of letters found
 
 	data resb 1024	; the data that will be read
+	chr resb 1		; the current character
 
 
 section .text
@@ -68,36 +72,64 @@ read_input:
 	
 
 main:
-	; printf(str_format, message);
-	mov rax, 0
-	mov rdi, str_fmt
-	mov rsi, msg
-	call printf
+	; startup
+	; sets up the secret word
+	mov QWORD [secret], temp
 
-	call read_input
 
-	mov rdi, msg
+	; sets up the length
+	mov rdi, [secret]
+	call len
+	mov DWORD [wlen], eax
+
+	; sets up the blind copy
+	mov rdi, [secret]
 	call blind_copy
-	push rax
+	mov QWORD [blind], rax
 
-	mov rdi, rax
-	mov rax, msg
-	mov sil, 'o'
+	mov DWORD [found], 0
+
+.loop:
+	; reads char
+	; prints character
+	call read_input
+	mov [chr], al
+	
+	xor rax, rax
+	mov rdi, chr_fmt
+	mov sil, [chr]
+	call printf
+
+
+	; checks if exists
+	; changes blind
+	mov rax, [secret]
+	mov rdi, [blind]
+	mov sil, [chr]
 	call dig_str
+	test rax, rax
+	jz .wrong
+	mov rdx, [found]
+	add rdx, rax
+	mov QWORD [found], rdx
+	jmp .post_wrong
+.wrong:
+	mov rdx, [tries]
+	add rdx, 1
+	mov [tries], rdx
+.post_wrong:
 
-	pop rsi
-	mov rax, 0
+	; prints blind
+	xor rax, rax
 	mov rdi, str_fmt
+	mov rsi, [blind]
 	call printf
 
-	mov rdi, 3
-	mov rsi, 5
-	call randint
-
-	mov rsi, rax
-	mov rdi, int_fmt
-	mov rax, 0
-	call printf
+	mov eax, [wlen]
+	mov edx, [found]
+	cmp edx, eax
+	jl .loop
+	
 
 	; exit(0);
 	mov rax, 60
