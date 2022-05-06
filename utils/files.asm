2177829bb75247d30bfc748a97a1caae79182473
romain.le-miere <romain.le-miere@epita.fr>
2022-05-06 15:44:54 +0200

    refactors line counting

--- utils/files.asm
+++ utils/files.asm
@@ -19,10 +19,12 @@ section .bss
 
 section .text
 
-get_word:
+count_lines:
+	; counts the number of lines
 	; gets a random word
 	; rbp-8: the file handle
-	; rbp-8: the number of bytes read
+	; rbp-12: the number of bytes read
+
 	push rbp
 	mov rbp, rsp
 	sub rsp, 12
@@ -37,7 +39,7 @@ get_word:
 	jle .exit
 	mov [rbp-8], rax
 
-.loop1:
+.loop:
 	; read the file and count the number of lines
 	; stores the number of bytes read
 	xor rax, rax		; read(
@@ -49,38 +51,25 @@ get_word:
 	; iterate over array
 	mov edi, eax
 	mov rax, buffer
-.loop1_1:
+.loop1:
 	test edi, edi
-	jz .exit1_1
+	jz .exit1
 	mov cl, [rax]
 	add rax, 1
 	sub edi, 1
 	; if (cl == "\n")
 	; 	counter++;
 	cmp cl, 0xa
-	jne .loop1_1
+	jne .loop1
 	mov rdx, [lcount]
 	add rdx, 1
 	mov [lcount], rdx
-	jmp .loop1_1
+	jmp .loop1
 	
-.exit1_1:
+.exit1:
 	mov eax, [rbp-12]
 	cmp eax, [buffsize]
-	jge .loop1
-
-	; generate randint between 0 and lcount-1
-	mov rdi, 0
-	mov rsi, [lcount]
-	call randint
-	mov [index], eax	; set the index of the word
-
-	; find the length of the word
-	; store the word on the buffer
-
-	; malloc the size of the word
-	
-	; fill the array with the word
+	jge .loop
 
 	; close the file
 	mov rax, 3			; close(
@@ -89,6 +78,48 @@ get_word:
 
 .exit:
 	; return the pointer to the word
+	mov rsp, rbp
+	pop rbp
+	ret
+
+	ret
+
+get_word:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 16
+	; rpb-8: the file handle
+	; rpb-16: the current word index
+	call count_lines
+	; generate randint between 0 and lcount-1
+	mov rdi, 0
+	mov rsi, [lcount]
+	call randint
+	mov [index], eax	; set the index of the word
+
+	mov QWORD [rbp-16], 0
+
+	; open the file
+	mov rax, 2			; open(
+	mov rdi, filename	;	filename,
+	mov rsi, 0			; 	O_RDONLY,
+	xor rdx, rdx		; 	WRITE
+	syscall				; )
+	cmp rax, 0
+	jle .exit
+	mov [rbp-8], rax
+
+.loop:
+	
+
+	; find the length of the word
+	; store the word on the buffer
+
+	; malloc the size of the word
+	
+	; fill the array with the word
+
+.exit:	
 	mov rsp, rbp
 	pop rbp
 	ret
